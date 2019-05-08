a712990fa26fb7db9d961a6d849dcb747e8124b1
vawohlsillo <vawohl@miuandes.cl>
2019-05-08 14:06:33 -0400

    write on console

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,15 +1,17 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 
 
-function hello(){
-console.log()
-}
+
 
 
 	
 $(document).ready(function(){
 	$("#hideShowButton").click(function(){
 		$("#kcontainer").toggle();
-		
 	});
+	$(".key").click(function(){
+		console.log($(this).text());
+	});
+	
+
 });
